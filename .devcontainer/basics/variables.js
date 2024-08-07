e9939243d97904e4675ef19ae47f3d59d327ced3
devtyagi26 <devashish.vatsa2003@gmail.com>
2024-08-07 07:32:04 +0000

    Configure done

--- .devcontainer/basics/variables.js
+++ .devcontainer/basics/variables.js
@@ -1,1 +1,14 @@
-console.log("Devashish");
+const accountid = 144553
+let accountemail = "devashish@google.com"
+var accountpass = "12345"
+accountcity = "Noida"
+
+// accountid = 2 
+
+accountemail = "dev@de.com"
+accountpass = "2314563"
+accountcity = "Pune"
+
+console.log(accountid);
+
+console.log([accountid, accountemail, accountpass, accountcity]);
