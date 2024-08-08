4956057e1a927efd37276e6d5948164ba166a1e9
devtyagi26 <devashish.vatsa2003@gmail.com>
2024-08-08 05:48:56 +0000

    conversions

---
+++ basic-1/conversions.js
@@ -0,0 +1,32 @@
+// let score = "33abc"
+// let score1 = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let value = Number(score)
+// console.log(typeof value);
+
+// console.log(value);
+// let value1 = Number(score1)
+// console.log(value1);
+
+// 33 = Number
+// 33abc = NaN
+// true = 1 and false = 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 = true and 0 = false
+// " " = false
+// "string" = true
+
+let num = 33
+
+let str = String(num)
+console.log(str);
+console.log(typeof str);
+
