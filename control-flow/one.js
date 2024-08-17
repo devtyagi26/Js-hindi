6179790dd58f5679d787388542eb847eb98cc8a4
devtyagi26 <devashish.vatsa2003@gmail.com>
2024-08-17 06:36:58 +0000

    control flow

---
+++ control-flow/one.js
@@ -0,0 +1,74 @@
+// if
+
+// if(condition //always should be true){
+//     then it is executed
+// }
+// if(condition // is false ){
+//     then it is not executed}
+
+// const isUserLoggedIn = true
+// if(isUserLoggedIn){
+
+// }
+
+
+// if(2 == "2"){
+//     console.log("executed");
+// }
+
+
+// const balance = 1000
+
+// if(balance < 500){
+//     console.log("less than");
+// } else if (balance < 750){
+//     console.log("less than 750");
+    
+// }else if (balance < 900){
+//     console.log("less than 900");
+    
+// }else {
+//     console.log("less than 1200");
+    
+// }
+
+
+// const userLoggedIn = true
+// const debitCard = true
+// const LoggedInFromGoogle = false
+// const LoggedInFromEmail = true
+
+
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy course");
+// }
+
+// if(LoggedInFromGoogle || LoggedInFromEmail){
+//     console.log("user logged in");
+    
+// }
+
+
+
+// ***********************  //
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Not matched");
+        
+        break;
+}
