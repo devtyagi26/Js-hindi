1e67a1cb96f197dd85c65354e23cc5b8f0b930db
devtyagi26 <devashish.vatsa2003@gmail.com>
2024-08-21 17:20:28 +0000

    do-while

---
+++ control-flow/do.js
@@ -0,0 +1,20 @@
+// let i = 0
+// while(i <= 10){
+//     console.log(`Value of i is: ${i}`);
+//     i = i+5
+// }
+
+
+let arr = ['flash', 'batman', 'shaktiman']
+let array = 0
+while(array < arr.length){
+    console.log(`value is ${arr[array]}`);
+    array = array + 1
+}
+
+
+let score = 11
+do{
+    console.log(`Score is ${score}`);
+    score++
+} while(score<=10);
