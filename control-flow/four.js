4ef11db18e78f57ac5370c660f431bf8912e3068
devtyagi26 <devashish.vatsa2003@gmail.com>
2024-08-27 17:42:53 +0000

    foreach

---
+++ control-flow/four.js
@@ -0,0 +1,22 @@
+const coding = ["js","ruby","java","cpp","python"]
+
+// coding.forEach(function (val){
+//     console.log(val);
+    
+// })
+
+// coding.forEach( (item) =>{
+//     console.log(item);
+
+// })
+
+// function print(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(print)
+
+// coding.forEach(item, index, arr) => {
+//     console.log(item,index,arr);
+    
+// }
