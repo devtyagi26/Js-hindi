dd58db2181fd8f936554ef1910eec0b3d087f983
devtyagi26 <devashish.vatsa2003@gmail.com>
2024-08-30 17:58:36 +0000

    Arrow-fun

---
+++ control-flow/five.js
@@ -0,0 +1,13 @@
+const mynum = [1,2,3,4,5,6,7,8,9,10]
+
+// const newnum = mynum.filter( (num)=> num > 4 )
+// console.log(newnum);
+
+// const num = mynum.filter( (num)=>{
+//     return num > 4
+// })
+// console.log(num);
+
+const newnum = mynum.map( (num) => num + 10)
+console.log(newnum);
+
